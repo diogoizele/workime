ba61f05b7dce2a548914f7a5d9c5c5bd4a5038d0
diogoizele <diogogabrielizele@gmail.com>
2023-01-15 19:09:39 -0300

    chore: add first project config relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            providers: "./src/providers",
+            screens: "./src/screens",
+            components: "./src/components",
+            styles: "./src/styles",
+          },
+        },
+      ],
+    ],
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
