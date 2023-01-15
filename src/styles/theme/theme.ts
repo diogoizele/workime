e30cea2c4bbaab48e076bf2e3c3eb69579204da7
diogoizele <diogogabrielizele@gmail.com>
2023-01-15 19:30:12 -0300

    feat: create and configure project theme

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    primary: "#553a78",
+
+    secondary: "#f6a207",
+    secondary_light: "rgba(246, 162, 7, 0.3)",
+
+    success: "#06b06b",
+    success_light: "rgba(6, 176, 107, 0.5)",
+
+    attention: "#E83F5B",
+    attention_light: "rgba(232, 63, 91, 0.5)",
+
+    white: "#FFFFFF",
+
+    title: "#363F5F",
+
+    text: "#969CB2",
+
+    text_dark: "#121314",
+
+    background: "#F0F2F5",
+  },
+  fonts: {
+    regular: "Inter_400Regular",
+    medium: "Inter_500Medium",
+    bold: "Inter_700Bold",
+  },
+};
